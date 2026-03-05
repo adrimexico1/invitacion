077ee12672d392feb8774cccd92d8cff3a028e76
adrimexico1 <adriansanchezcontreras87@gmail.com>
2026-03-05 15:56:12 -0600

    Actualizada URL de Google Script y limpieza de invitados.json

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwIk0rPj5l0HboY_j6CP7bmoDSiM9wRbGSNJQ-YyS7zGhi_KdseRzAseeo9p1eNZ6Aj/exec'
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyQgN7qA2jZ-otPzpOTcA-iFcT6rbDfRDbNghCYoZyDFbzPVs440PwVL3sFbfYTs008/exec'
 };
